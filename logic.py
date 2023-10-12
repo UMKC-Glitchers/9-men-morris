e826897e5048ac24e94b51e2d801deb8e5770cbc
rmf9t <rmf9t@umsystem.edu>
2023-10-12 00:48:50 -0500

    Adding make move method

--- logic.py
+++ logic.py
@@ -6,22 +6,35 @@ class NineMensMorrisGame:
         self.phase = constants.PHASE1
         self.turn = constants.PLAY1
         self.CURRENT_POSITION = constants.CURRENT_POSITION
+        self.counter = 0
+        self.play1_counter = 0
+        self.play2_counter = 0
 
     def change_turn(self):
         if self.turn == constants.PLAY1:
+            self.play1_counter = self.play1_counter + 1
             self.turn = constants.PLAY2
         else:
+            self.play2_counter = self.play2_counter + 1
             self.turn = constants.PLAY1
+
+        if self.play1_counter == constants.TOTAL_MENS and self.play2_counter == constants.TOTAL_MENS:
+            self.set_phase(constants.PHASE2)
+
 
     def get_turn(self):
         return self.turn
+
+    def set_phase(self, phase):
+        self.phase = phase
 
     def make_move(self, row, col, new_row, new_col):
         valid = self.is_move_valid(row, col, new_row, new_col, None)
         print("clicked", row + 1, col + 1, valid)  # Debug message, Remove it later
         if valid:
+            self.counter = self.counter + 1
             if self.phase == constants.PHASE1:
-                self.place_piece(row, col, self.turn)
+                self.place_piece(row, col, self.get_turn())
                 self.change_turn()
             elif self.phase == constants.PHASE2:
                 pass
@@ -42,11 +55,12 @@ class NineMensMorrisGame:
         if (self.phase == constants.PHASE1) or (self.phase == constants.PHASE3):
             return [row, col] in self.get_valid_moves()
         elif self.phase == constants.PHASE2:
-            for move in moves:
-                newb1 = str(move[0]) + str(move[1])
-                newb2 = str(new_row) + str(new_col)
-                if newb1 == newb2:
-                    return 1
+            # TODO - Implement me
+            # for move in moves:
+            #     newb1 = str(move[0]) + str(move[1])
+            #     newb2 = str(new_row) + str(new_col)
+            #     if newb1 == newb2:
+            #         return 1
             return 0
 
     def get_valid_moves(self):
