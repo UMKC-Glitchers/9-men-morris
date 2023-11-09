d023df0b52dcfd1ebb7bc6ccdecc8f0da7d3551f
rmf9t <rmf9t@umsystem.edu>
2023-11-08 20:25:14 -0600

    Adding new menu for game

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from GUI import NineMensMorrisGUI
 from logic import NineMensMorrisGame
+from menu import Menu
+
 
 if __name__ == '__main__':
-    game = NineMensMorrisGame()
+    menu = Menu()
+    menu.display_menu()
+    selected_option = menu.get_selected_option()
+
+    if selected_option == 'human_vs_human':
+        game = NineMensMorrisGame()
+    elif selected_option == 'human_vs_computer':
+        # Initialize game for human vs computer mode
+        game = NineMensMorrisGame()  # Replace with your computer player logic
+
     gui = NineMensMorrisGUI(game)
     gui.main_loop()
